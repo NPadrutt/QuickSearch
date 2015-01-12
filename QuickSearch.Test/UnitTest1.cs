11b89f28927b5ad11cab6ab3266415d7ea992ff4
NPadrutt <nino.padrutt@hotmail.com>
2015-01-12 13:10:08 +0100

    implement a quick search

---
+++ QuickSearch.Test/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuickSearch.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void QuickSearch_PatternMatch()
+        {
+            var pattern = "abac";
+            var text = "asfdasdföjnaögnabacwiwerölewanrö";
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_PatternMatchStart()
+        {
+            var pattern = "abac";
+            var text = "abacasfdasdföjnaögnaacwiwerölewanrö";
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_PatternMatchEnd()
+        {
+            var pattern = "abac";
+            var text = "asfdasdföjnaögnabawiwerölewanröabac";
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_PatternNotMatch()
+        {
+            var pattern = "abac";
+            var text = "asfdasdföjnaögnaacwiwerölewanrö";
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_PatternLargerText()
+        {
+            var pattern = "asfdasdföjnaögnaacwiwerölewanrö";
+            var text = "abac";
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_EmptyText()
+        {
+            var pattern = "abac";
+            var text = String.Empty;
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_EmptyPattern()
+        {
+            var pattern = String.Empty;
+            var text = "asfdasdföjnaögnaacwiwerölewanrö";
+            int expected = -1;
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_EmptyPatternAndText()
+        {
+            var pattern = String.Empty;
+            var text = String.Empty;
+            int expected = -1;
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuickSearch_PatternMatch_SpecialChars()
+        {
+            var pattern = "ﻈﻲﻧﺫ﴿";
+            var text = "ﺝﺝﷴﺠ꞉■ﻈﻲﻧﺫ﴿ﺳﺷﺱﯯﻋ₽─∞";
+            int expected = text.IndexOf(pattern);
+
+            int actual = Program.DoQuickSearch(pattern, text);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ QuickSearch/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace QuickSearch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int DoQuickSearch(string pattern, string text)
+        {
+            if (String.IsNullOrEmpty(pattern) || String.IsNullOrEmpty(text))
+            {
+                return -1;
+            }
+
+            // pattern = abac
+            var p = pattern.ToCharArray();
+            // text = werjewjabaczwzer
+            var a = text.ToCharArray();
+
+            var pi = 0;
+            var ai = 0;
+            var pl = p.Length;
+
+            // shift = .... abcdefg...
+            var shift = new int[Char.MaxValue];
+            var initShiftValue = pl + 1;
+
+            // shift = 5,5,5,5,5,5
+            for (var i = 0; i < shift.Length; i++)
+            {
+                shift[i] = initShiftValue;
+            }
+
+            for (var i = 0; i < pl; i++)
+            {
+                shift[p[i]] = pl - i;
+            }
+            
+            while(a.Length - ai >= pl - pi)
+            {
+                if (a[ai] == p[pi])
+                {
+                    if (pl == pi + 1)
+                    {
+                        return ai -(pl-1);
+                    }
+
+                    pi++;
+                    ai++;
+                }
+                else
+                {
+                    ai += shift[a[ai + pl - pi]];
+                    pi = 0;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
